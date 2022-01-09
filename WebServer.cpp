6199d7abf11cc5c447552ce8fb50d396d163cb2c
YHTerrance <b08902073@ntu.edu.tw>
2022-01-10 00:15:27 +0800

    Add local webserver which will act as the local web client in the future (currently serves static small text files to browsers only)

---
+++ WebServer.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <istream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <streambuf>
+#include <iterator>
+#include "WebServer.hpp"
+
+// Handler for when a message is received from the client
+void WebServer::on_message_received(int client_socket, const char *msg, int length) {
+
+    // Parse out the document requested (gives all strings separated by spaces)
+    std::istringstream iss(msg);
+    std::vector<std::string> parsed((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
+
+
+    std::string content = "<h1>404 Not Found</h1>";
+    std::string html_file = "/index.html";
+    int statusCode = 404;
+
+    if (parsed.size() >= 3 && parsed[0] == "GET") {
+        html_file = parsed[1];
+
+        if(html_file == "/")
+            html_file = "/index.html";
+    }
+
+    // Open the document in the local file system
+    std::ifstream f("./wwwroot" + html_file);
+
+    if (f.good()) {
+        std::string str((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
+        content = str;
+        statusCode = 200;
+    }
+
+    f.close();
+
+    // Write the document back to the client
+    std::ostringstream oss;
+    oss << "HTTP/1.1 " << statusCode << " OK\n";
+    oss << "Cache-Control: no-cache, private\n";
+    oss << "Content-Type: text/html\n";
+    oss << "Content-Length: " << content.size() << "\n";
+    oss << "\n";
+    oss << content;
+
+    std::string output = oss.str();
+    int size = output.size() + 1;
+
+    send_to_client(client_socket, output.c_str(), size);
+}
+
+// Handler for client connections
+void WebServer::on_client_connected(int client_socket) {
+
+}
+
+// Handler for client disconnections
+void WebServer::on_client_disconnected(int client_socket) {
+
+}
---
+++ WebServer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "TcpListener.hpp"
+
+class WebServer : public TcpListener {
+    public:
+        WebServer(const char* ip_address, int port) :
+            TcpListener(ip_address, port) { }
+
+    protected:
+        // Handler for when a message is received from the client
+        virtual void on_message_received(int client_socket, const char *msg, int length);
+
+        // Handler for client connections
+        virtual void on_client_connected(int client_socket);
+
+        // Handler for client disconnections
+        virtual void on_client_disconnected(int client_socket);
+
+    private:
+
+};
